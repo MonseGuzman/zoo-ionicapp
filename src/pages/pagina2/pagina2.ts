ed37a3a7cebcfc77887ce94968e7a4450815b094
Monse Guzmán <andreaguzman.lopez23@gmail.com>
2019-02-11 00:57:53 -0600

    added interface and data for this page

--- src/pages/pagina2/pagina2.ts
+++ src/pages/pagina2/pagina2.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Pagina3Page } from '../index.pages';
+import { Animal } from '../../interfaces/animal.interface';
+import { ANIMALES } from '../../data/data.animales';
 
 @IonicPage()
 @Component({
@@ -9,44 +11,12 @@ import { Pagina3Page } from '../index.pages';
 })
 export class Pagina2Page {
 
-  animales: any[] = [
-    {
-      nombre: "Caballo",
-      foto: "assets/animalitos/caballito.jpg"
-    },
-    {
-      nombre: "Cabra",
-      foto: "assets/animalitos/cabrita.jpg"
-    },
-    {
-      nombre: "Cerdo",
-      foto: "assets/animalitos/cerdito.jpg" 
-    },
-    {
-      nombre: "Gallo",
-      foto: "assets/animalitos/gallito.jpg" 
-    },
-    {
-      nombre: "Mono",
-      foto: "assets/animalitos/monito.jpg" 
-    },
-    {
-      nombre: "Perro",
-      foto: "assets/animalitos/perrito.jpeg" 
-    },
-    {
-      nombre: "Serpiente",
-      foto: "assets/animalitos/serpiente.jpg" 
-    },
-    {
-      nombre: "Tigre",
-      foto: "assets/animalitos/tigre.png" 
-    },
-  ];
+  animales: Animal[] = [];
 
   pagina3: any = Pagina3Page;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.animales = ANIMALES.slice(0);
   }
 
   irPagina3(animal:any){
